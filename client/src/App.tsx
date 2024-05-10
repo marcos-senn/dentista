5e98adca899309a611bd585c8a61d82dae795295
Marcos Antonio Senn <marcossenn@gmail.com>
2024-05-10 14:41:16 -0300

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from './components/Header'
+
+function App() {
+    return (
+        <>
+            <Header></Header>
+        </>
+    );
+}
+
+export default App;
